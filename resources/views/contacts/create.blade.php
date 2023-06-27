ea91268e067a072154aae0bf143ca3b8b7a42e0d
Renat <renat.junusov@inbox.lv>
2023-06-27 19:34:27 +0300

    Added vallidation.

--- resources/views/contacts/create.blade.php
+++ resources/views/contacts/create.blade.php
@@ -16,20 +16,20 @@
         <form action="{{ route('contacts.store') }}" method="POST">
             @csrf
             <div class="addbox"> 
-            <div class="form-group">
-                <label for="name" class="nametext">Name</label>
-                <input type="text" name="name" id="name" class="form-control" required>
-            </div>
-            <div class="form-group">
-                <label for="phone_number" class="phonetext" >Phone Number</label>
-                <input type="text" name="phone_number" id="phone_number" class="form-control" required>
-            </div>
-            <div class="form-group">
-                <label for="email" class="emailtext">Email</label>
-                <input type="email" name="email" id="email" class="form-control" required>
-            </div>
-            <!-- Add other relevant fields -->
-            <button type="submit" class="btn btn-primary">Add</button>
+                <div class="form-group">
+                    <label for="name" class="nametext">Name</label>
+                    <input type="text" name="name" id="name" class="form-control" required>
+                </div>
+                <div class="form-group">
+                    <label for="phone_number" class="phonetext">Phone Number</label>
+                    <input type="text" name="phone_number" id="phone_number" class="form-control" pattern="\d*" required>
+                </div>
+                <div class="form-group">
+                    <label for="email" class="emailtext">Email</label>
+                    <input type="email" name="email" id="email" class="form-control" required>
+                </div>
+                <!-- Add other relevant fields -->
+                <button type="submit" class="btn btn-primary">Add</button>
             </div>
         </form>
     </div>
